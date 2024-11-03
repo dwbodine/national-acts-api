7a010b5e3c072d1dfea73d8856f428500edf0227
Dennis Bodine <dbodine@tfin.com>
2024-11-03 16:08:50 -0600

    #54321 linting for user

--- common/models/user.py
+++ common/models/user.py
@@ -1,82 +1,117 @@
+"""
+User models
+"""
+
+
 class Permission:
-    def __init__(self, permissionId: int, permissionName: str):
-        self.permissionId = permissionId
-        self.permissionName = permissionName
+    """
+    Single permission in system
+    """
+
+    def __init__(self, permission_id: int, permission_name: str):
+        self.permission_id = permission_id
+        self.permission_name = permission_name
 
 
 class Role:
+    """
+    Single role with attached permissions in system
+    """
+
     permissions: list[Permission] = []
 
-    def __init__(self, roleId: int, roleName: str):
-        self.roleId = roleId
-        self.roleName = roleName
+    def __init__(self, role_id: int, role_name: str):
+        self.role_id = role_id
+        self.role_name = role_name
 
 
 class UserSeller:
+    """
+    User seller with attached permissions
+    """
+
     permissions: list[int] = []
 
     def __init__(
         self,
-        userSellerId: int,
-        sellerId: int,
-        sellerName: str,
-        sellerType: int,
-        roleId: int,
+        user_seller_id: int,
+        seller_id: int,
+        seller_name: str,
+        seller_type: int,
+        role_id: int,
     ):
-        self.userSellerId = userSellerId
-        self.sellerId = sellerId
-        self.sellerName = sellerName
-        self.sellerType = sellerType
-        self.roleId = roleId
+        self.user_seller_id = user_seller_id
+        self.seller_id = seller_id
+        self.seller_name = seller_name
+        self.seller_type = seller_type
+        self.role_id = role_id
 
 
 class UserActivity:
+    """
+    User activity object
+    """
+
     def __init__(
         self,
-        userId: int,
-        activityType: int,
-        activityData: str,
-        activityTime: str,
-        activityName: str,
+        user_id: int,
+        activity_type: int,
+        activity_data: str,
+        activity_time: str,
+        activity_name: str,
         username: str,
     ):
-        self.userId = userId
-        self.activityType = activityType
-        self.activityData = activityData
-        self.activityTime = activityTime
-        self.activityName = activityName
+        self.user_id = user_id
+        self.activity_type = activity_type
+        self.activity_data = activity_data
+        self.activity_time = activity_time
+        self.activity_name = activity_name
         self.username = username
 
 
 class User:
-    userId: int = 0
-    isAdmin: bool = False
+    """
+    User object
+    """
+
+    user_id: int = 0
+    is_admin: bool = False
     username: str = None
     password: str = None
-    isAuthenticated: bool = False
-    firstName: str = None
-    lastName: str = None
+    is_authenticated: bool = False
+    first_name: str = None
+    last_name: str = None
     mobile: str = None
     notes: str = None
-    isActive: bool = False
-    createdAt: str = None
+    is_active: bool = False
+    created_at: str = None
     token: str = None
     category: str = None
-    requireResetPassword: bool = False
-    lastUpdate: str = None
-    sendEmailReset: bool = False
-    sendTextReset: bool = False
-    disableCheckIn: bool = False
+    require_reset_password: bool = False
+    last_update: str = None
+    send_email_reset: bool = False
+    send_text_reset: bool = False
+    disable_check_in: bool = False
     sellers: list[UserSeller] = []
 
-    def userFullname(self):
-        return self.firstName + " " + self.lastName + " (" + self.username + ")"
+    def user_full_name(self):
+        """
+        Convenience method to return user's full name
+        """
+        return self.first_name + " " + self.last_name + " (" + self.username + ")"
 
 
 class UserResponse:
-    def __init__(self, user: User, errorMessage: str = None):
-        self.user = user
-        self.errorMessage = errorMessage
+    """
+    User response object for API calls
+    """
 
-    def hasError(self):
-        return self.errorMessage is not None and self.errorMessage != ""
+    def __init__(self, user: User, error_message: str = None):
+        self.user = user
+        self.error_message = error_message
+
+    def has_error(self):
+        """
+        Returns true/false if error message is present
+        """
+        return self.error_message is not None and self.error_message != ""
